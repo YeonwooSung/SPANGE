138e0d99096c75cfae3329462aa755ceecc8ab7d
YeonwooSung <neos960518@gmail.com>
2020-07-27 13:27:37 +0900

    Update app icon, splash screen, and add manager screen

---
+++ SPANGE/app/src/main/java/com/technonia/spange/ManagerActivity.java
@@ -0,0 +1,22 @@
+package com.technonia.spange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager);
+    }
+
+    private void getUserList() {
+        //
+    }
+
+    private void initScreen() {
+        //
+    }
+}
